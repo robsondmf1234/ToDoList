5dd04c5882e1701a4318443b4a6d9691305a9864
robsondmf <robsondmf1234@gmail.com>
2024-08-11 20:52:19 -0300

    feat - Update layout.

--- app/src/main/java/com/example/listadetarefas/fragments/AddFragment.kt
+++ app/src/main/java/com/example/listadetarefas/fragments/AddFragment.kt
@@ -2,10 +2,16 @@ package com.example.listadetarefas.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import com.example.listadetarefas.R
 import com.example.listadetarefas.databinding.FragmentAddBinding
 
@@ -20,7 +26,59 @@ class AddFragment : Fragment() {
     ): View {
         binding = FragmentAddBinding.inflate(inflater, container, false)
         setupListeners()
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val menuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.add_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    //R.id.menu_add -> insertDataDb()
+                    R.id.menu_add -> prepareToSaveInformations()
+                    android.R.id.home -> requireActivity().onBackPressed()
+                }
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.STARTED)
+    }
+
+/*    private fun insertDataDb() {
+
+        val mTitle = binding.titleEt.text.toString()
+        val mPriority = binding.prioritiesSpinner.selectedItem.toString()
+        val mDescription = binding.descriptionEt.text.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
+        if (validation) {
+            //Insert Data to Database
+            val newData = ToDoData(
+                0,
+                mTitle,
+                mSharedViewModel.parsePriorityToInt(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
+            Toast.makeText(requireContext(), "Successfuly added", Toast.LENGTH_SHORT).show()
+            //Navigate back
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all field.", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }*/
+
+    private fun prepareToSaveInformations() {
+        val (description, favorite) = colectInformations()
+        showInformations(description, favorite)
     }
 
     private fun setupListeners() {
@@ -28,11 +86,12 @@ class AddFragment : Fragment() {
             toggleFavorite()
             checkFavorite()
         }
-        binding.saveBtn.setOnClickListener {
-            val description = binding.descriptionEt.text.toString()
-            val favorite = isFavorite
-            showInformations(description, favorite)
-        }
+    }
+
+    private fun colectInformations(): Pair<String, Boolean> {
+        val description = binding.descriptionEt.text.toString()
+        val favorite = isFavorite
+        return Pair(description, favorite)
     }
 
     private fun checkFavorite() {
